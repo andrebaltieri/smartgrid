35cc80d4317d9ff4ac18363eb2f55a0cdd4fd8bf
André Baltieri <andrebaltieri@hotmail.com>
2014-05-31 10:43:36 -0300

    Interation through columns

--- src/js/jquery.smartgrid.js
+++ src/js/jquery.smartgrid.js
@@ -1,12 +1,28 @@
-(function($) {
- 
-    $.fn.smartGrid = function(columns) {
+(function ($) {
+
+    $.fn.smartGrid = function (params) {
+        var grid = '';
         
-        columns.each(function(index, value) {
-            $(this).append("<li>" + value + "</li>");
-        });
-        
+        grid += '<table class="table">';
+        // Generate table header
+        grid += generateHeader(params.columns);
+        grid += '</table>';
+
+        $(this).append(grid);
         return this;
     };
- 
+
 }(jQuery));
+
+function generateHeader(columns) {
+    var htmlContent = '';
+    htmlContent += '<thead>';
+    htmlContent += '<tr>';
+    $(columns).each(function () {
+        htmlContent += '<th>' + this.name + '</th>';
+    });
+    htmlContent += '</tr>';
+    htmlContent += '</thead>';
+
+    return htmlContent;
+}
